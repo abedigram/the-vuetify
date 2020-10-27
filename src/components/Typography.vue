9618a9b5a5a60b73a0af040122393edf769caae5
baQerial <mbapost@yahoo.com>
2020-10-28 00:31:35 +0330

    typography trained

--- src/components/Typography.vue
+++ src/components/Typography.vue
@@ -3,10 +3,10 @@
     <h3>Typography</h3>
     <v-card class="mt-5 mb-8">
       <v-card-text>
-        <h1 class="display-4">Heading 1</h1>
-        <h2 class="display-3">Heading 2</h2>
-        <h3 class="display-2">Heading 3</h3>
-        <h4 class="title">Title</h4>
+        <h1 class="display-4 purple font-weight-black font-italic text-truncate">lorem ipsum sit dolor ametjoing</h1>
+        <h2 class="display-3 red--text text-uppercase">Heading 2</h2>
+        <h3 class="display-2 yellow--text text--darken-4">Heading 3</h3>
+        <h4 class="title text--primary">Title</h4>
         <h5 class="subtitle-1">Subtitle</h5>
         <p class="body-1">Body</p>
       </v-card-text>
